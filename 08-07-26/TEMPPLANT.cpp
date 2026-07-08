f6b03807ff402b295680bee6928de044cb9e6dd6
Mohit <162956586+gthub-mohit@users.noreply.github.com>
2026-07-08 20:07:52 +0530

    Solved TEMPPLANT [C++​]

---
+++ 08-07-26/TEMPPLANT.cpp
@@ -0,0 +1,31 @@
+/*
+ ╔═══════════════════════════════════════════════════════════════════════╗
+ ║  Problem  : TEMPPLANT                                                   ║
+ ║  Platform : CodeChef                                                  ║
+ ║  Status   : Accepted                                                    ║
+ ║  Date     : July 8, 2026                                                ║
+ ║  URL      : https://www.codechef.com/START246C/problems/TEMPPLANT       ║
+ ╚═══════════════════════════════════════════════════════════════════════╝
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i;
+    cin>>i;
+    while(i--){
+        int n;
+        cin>>n;
+        vector<int> arr(n);
+        for(int k=0;k<n;k++){
+            cin>>arr[k];
+        }
+        int maxi=0;
+        for(int k=0;k<n;k++){
+            int curr=min(arr[k+1] , arr[k]);
+            maxi=max(maxi  , curr);
+        }
+        cout<<maxi<<endl;
+    }
+    return 0;
+}
