6124ede2f43e178a6031db4caf2d634dbe1e2854
Mohit <162956586+gthub-mohit@users.noreply.github.com>
2026-08-05 20:11:13 +0530

    Solved BISHOPMV [C++​]

---
+++ 05-08-26/BISHOPMV.cpp
@@ -0,0 +1,24 @@
+/*
+ ╔═══════════════════════════════════════════════════════════════════════╗
+ ║  Problem  : BISHOPMV                                                    ║
+ ║  Platform : CodeChef                                                  ║
+ ║  Status   : Accepted                                                    ║
+ ║  Date     : August 5, 2026                                              ║
+ ║  URL      : https://www.codechef.com/START250C/problems/BISHOPMV        ║
+ ╚═══════════════════════════════════════════════════════════════════════╝
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i;
+    cin>>i;
+    while(i--){
+        int x1 , x2 , y1 , y2;
+        cin>>x1>>y1>>x2>>y2;
+        if(abs(x1-y1)==abs(x2-y2))cout<<1<<endl;
+        else if((x1+y1)%2!=(x2+y2)%2)cout<<-1<<endl;
+        else cout<<2<<endl;
+    }
+    return 0;
+}
