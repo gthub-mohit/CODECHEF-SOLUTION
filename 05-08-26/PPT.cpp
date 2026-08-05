dc3e0c453f4c4193376b69f2358edea180dbd1bd
Mohit <162956586+gthub-mohit@users.noreply.github.com>
2026-08-05 20:16:47 +0530

    Solved PPT [C++​]

---
+++ 05-08-26/PPT.cpp
@@ -0,0 +1,18 @@
+/*
+ ╔═══════════════════════════════════════════════════════════════════════╗
+ ║  Problem  : PPT                                                         ║
+ ║  Platform : CodeChef                                                  ║
+ ║  Status   : Accepted                                                    ║
+ ║  Date     : August 5, 2026                                              ║
+ ║  URL      : https://www.codechef.com/START250C/problems/PPT             ║
+ ╚═══════════════════════════════════════════════════════════════════════╝
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    cout<<(600-(n*30))/30<<endl;
+    return 0;
+}
