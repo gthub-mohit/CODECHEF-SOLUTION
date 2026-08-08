311441422fdc293219de438aa0918758eef385a6
Mohit <162956586+gthub-mohit@users.noreply.github.com>
2026-08-08 16:14:14 +0530

    Solved OG [C++​]

---
+++ 08-08-26/OG.cpp
@@ -0,0 +1,26 @@
+/*
+ ╔═══════════════════════════════════════════════════════════════════════╗
+ ║  Problem  : OG                                                          ║
+ ║  Platform : CodeChef                                                  ║
+ ║  Status   : Accepted                                                    ║
+ ║  Date     : August 8, 2026                                              ║
+ ║  URL      : https://www.codechef.com/problems/OG                        ║
+ ╚═══════════════════════════════════════════════════════════════════════╝
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int i;
+    cin>>i;
+    while(i--) {
+        long long n;
+        cin>>n;
+        long long q = n/9;
+        long long r = n%9;
+        long long ans =q*45+r*(r+1)/2;
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
