08f984d5800ae93c0b1b339d992cc908792c650d
Mohit <162956586+gthub-mohit@users.noreply.github.com>
2026-08-24 18:30:32 +0530

    Solved ARYAGRID on CodeChef [C++​]

---
+++ 2026-08/ARYAGRID.cpp
@@ -0,0 +1,24 @@
+/*
+ ╔═══════════════════════════════════════════════════════════════════════╗
+ ║  Problem  : ARYAGRID                                                    ║
+ ║  Platform : CodeChef                                                    ║
+ ║  Status   : Accepted                                                    ║
+ ║  Date     : August 24, 2026                                             ║
+ ║  URL      : https://www.codechef.com/problems/ARYAGRID?tab=statement    ║
+ ╚═══════════════════════════════════════════════════════════════════════╝
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i;
+    cin>>i;
+    while(i--){
+        int n , m , x , y , l;
+        cin>>n>>m>>x>>y>>l;
+        long long rows = 1+(x-1)/l+(n-x)/l;
+        long long cols = 1+(y-1)/l+(m-y)/l;
+        cout<<rows*cols<<endl;
+    }
+    return 0;
+}
